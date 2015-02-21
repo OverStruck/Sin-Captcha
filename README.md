00dee70454769348a1ca1e69e7d69821b75acc37
NIX <nix@juanix.net>
2015-02-20 20:36:29 -0800

    First upload

---
+++ README.md
@@ -0,0 +1,56 @@
+#Sin Captcha (Para Español leer mas abajo)
+*Sin Captcha* is a very simple PHP class use to generate a type of "captcha" that's not really a captcha.
+It basically allows you to add anti-bot type of check very easy.
+
+![Screenshot](http://i.imgur.com/Im3bwLl.png)
+
+It generates buttons with numbers and selects a number as the answer.
+If the user clicks the correct number, you then, as the programmer, decide what to do or not do.
+
+![Screenshot](http://i.imgur.com/IKQb8Qu.png)
+
+###You can set:
+* A minimum/maximum number to use
+* The number of buttons to show/generate (defaults to 4)
+* The type of button: <input> or <button>
+* A class attribute for the buttons
+* A name attribute for the buttons
+
+You just need to put the generated buttons in a form or use Ajax.
+
+Check the example files to see a simple example on how to use this class
+
+*Any feedback is appreciated. If you have any suggestions to improve this script, don't hesitate to let me know. If you are a developer or student that knows programming, feel free to add features or fix stuff.*
+
+###How to use
+Just upload all the files to your server and the script should be functional. There's no installation needed or a data base.
+
+#Español
+*Sin Captcha* es un script (una clase de PHP) muy simple para generar una especie de "captcha" que en realidad no es un captcha.
+Básicamente, te permite añadir un tipo de anti-bot muy fácil.
+
+![Screenshot](http://i.imgur.com/Im3bwLl.png)
+
+Genera botones con números y selecciona un número como respuesta.
+Si el usuario hace clic en el número correcto, entonces tu, como el programador, decides qué hacer o no hacer.
+
+![Screenshot](http://i.imgur.com/IKQb8Qu.png)
+
+### Puedes configurar:
+* Un número mínimo / máximo a usar
+* El número de botones para mostrar / generar (por defecto genera 4)
+* El tipo de botón: <input> o <button>
+* El atributo "class" para los botones
+* El atributo "name" para los botones
+
+Sólo tiene que poner los botones que se generan en un formulario o utilizar Ajax.
+
+Mira los archivos de ejemplo para ver un ejemplo sencillo de cómo utilizar esta clase.
+
+*Cualquier consejo o comentario es bienvenido. Si eres un programador o un estudiante, siéntete libre de agregar funciones o reparar bugs.*
+
+###Que esta pasando ahora?
+Como no tengo mucho tiempo, pulir este script sera un proceso lento que probablemente haré en mi tiempo libre cuando tenga ganas. Simplemente decidí subir este script para aquellas personas que estén interesadas en algo así.
+
+###Como usar
+Simplemente sube los archivos a tu servidor web y el script estará listo para usarse. No requiere ningún tipo de instalación o bases de datos.
---
+++ sin-captcha.class.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * SinCaptcha class
+ *
+ * Sin Captcha provee una manera fácil de crear un sistema parecido a los captcha.
+ * Sin Captcha genera X números al azar, selecciona uno como respuesta, también al azar
+ * Y genera los botones necesarios para poner en la forma de sumisión.
+ *
+ * @author Juanix <info@juanix.net>
+ * @copyright 2013 Juanix.net
+ * @license http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+class SinCaptcha {
+	/**
+	 * @var private array el HTML de los botones soportados
+	 */
+	private $btn_templates = array(
+		'button' => '<button type="submit" class="[class]" value="[val]" name="sin-captcha">[val]</button>', 'submit' => '<input type="submit" class="[class]" value="[val]" name="sin-captcha">'
+	);
+	
+	/**
+	 * Constructor
+	 * @param array $config la configuracion del captcha
+	 */
+	public function __construct($config = array()) {
+		
+		//la configuracion por defecto
+		$default_config = array(
+			'max_num' => 99,				//maximo numbero
+			'min_num' => 1,					//minimo numero
+			'numb_of_btns' => 4,			//numero de botones
+			'btn_type' => 'button',			//tipo de boton (<buton> o <input>)
+			'btn_name' => 'sin-captcha',	//nombre del boton (name=)
+			'btn_class' => ''				//clase del boton (class=)
+		);
+		
+		//si el user especifica alguna configuracion
+		//esta sobre-escribira la config por defecto
+		$config = array_merge($default_config, $config);
+		
+		//asigna configuracion a variables locales
+		foreach ($config as $key => $value) {
+			$this->$key = $value;
+		}
+	}
+	
+	/**
+	 * Genera los datos necesarios para la verificacion
+	 *
+	 * @return array La respuesta correcta y el HTML de los botones
+	 */
+	public function generate() {
+		$answer = ''; //respuesta
+		while (empty($answer)) {
+			$number = rand(1, $this -> numb_of_btns);
+			$numbers = $this -> getRandomNums();
+            $answer = $numbers[$number];
+		}
+		return array('answer' => $answer, 'buttons' => $this -> populateBtns($numbers));
+
+	}
+
+	/**
+	 * Genera el HTML de los botones
+	 *
+	 * @param int array $numbers los numeros generados unicos
+	 * @return string $btns el HTML de los botones
+	 */
+	private function populateBtns($numbers) {
+		$btn_template = $this -> btn_templates[$this -> btn_type];
+		$btns = '';
+		for ($i = 0; $i < $this -> numb_of_btns; $i++) {
+			$btns .= preg_replace('/\[val\]/', $numbers[$i], $btn_template);
+		}
+		$btns = preg_replace('/\[class\]/', $this -> btn_class, $btns);
+		return $btns;
+	}
+
+	/**
+	 * Genera X numeros al azar
+	 *
+	 * @return int array $randoms Los numeros generados
+	 */
+	private function getRandomNums() {
+		$randoms = array();
+		while (count($randoms) < $this -> numb_of_btns) {
+			$random = rand($this -> min_num, $this -> max_num);
+			if (!in_array($random, $randoms)) {
+                array_push($randoms, $random);
+			}
+		}
+		return $randoms;
+	}
+
+}
+?>
